e99dd37bdfa7e04e57fdecbc6ff518e15872eff6
s0ftik3 <slava021323@gmail.com>
2020-10-17 01:39:01 +0600

    Fix time issue.

--- src/scripts/sendBugReport.js
+++ src/scripts/sendBugReport.js
@@ -6,8 +6,8 @@ module.exports = (error) => {
     let day = date.getDate();
     let month = date.getMonth();
     let year = date.getFullYear();
-    let hour = Number(date.getHours()) + 6;
-    let min = date.getMinutes();
+    let hour = Number(date.getHours()).toString().padStart(2, '0');
+    let min = date.getMinutes().toString().padStart(2, '0');
 
     let message = `*Name:* ${error.name}\n*Message:* ${error.message}\n*Date:* ${day}/${month}/${year} ${hour}:${min}`;
     telegram.sendMessage(process.env.ADMIN, message, { parse_mode: 'Markdown' });
