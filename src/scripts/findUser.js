62977aba44327f5f80228dd05548aad671b66857
s0ftik3 <slava021323@gmail.com>
2020-10-17 21:24:09 +0600

    Minor updates.

--- src/scripts/findUser.js
+++ src/scripts/findUser.js
@@ -3,7 +3,8 @@ const User = require('../models/User');
 module.exports = async (id) => {
     // let result = await User.find({ id: id }).then(user => (user.length > 0) ? true : false).catch(e => console.error(e))
     let result = await User.find({ id: id }, (err, res) => {
-        return res.length;
+        let amount = res.length;
+        return amount;
     });
     return result;
 }
