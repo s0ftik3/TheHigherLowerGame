4cffb89d21ac27b660832736e0064bc509ef6c65
s0ftik3 <slava021323@gmail.com>
2020-10-17 21:28:40 +0600

    Minor updates.

--- src/scripts/findUser.js
+++ src/scripts/findUser.js
@@ -3,9 +3,9 @@ const User = require('../models/User');
 module.exports = async (id) => {
     // let result = await User.find({ id: id }).then(user => (user.length > 0) ? true : false).catch(e => console.error(e))
     let result = await User.find({ id: id }, (err, res) => {
-        return res.length;
+        return res;
     });
-    if (result > 0) {
+    if (result.length > 0) {
         return true;
     } else {
         return false;
