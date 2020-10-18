f4525fca23a96130c3b10880ca71375e68acb221
s0ftik3 <slava021323@gmail.com>
2020-10-18 21:17:56 +0600

    Minor bug fixes.

--- src/commands/game.js
+++ src/commands/game.js
@@ -1,6 +1,7 @@
 const TheHigerLowerGame = require('../game/TheHigherLower');
 const game = new TheHigerLowerGame();
 const sendBugReport = require('../scripts/sendBugReport');
+const User = require('../models/User');
 
 module.exports = () => (ctx) => {
     try {
