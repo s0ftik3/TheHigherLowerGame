ef9516a543aeb4f937df6103780329be0d3bd056
s0ftik3 <slava021323@gmail.com>
2020-10-24 23:03:05 +0600

    Minor bugfixes.

--- src/scripts/notifyAdmin.js
+++ src/scripts/notifyAdmin.js
@@ -3,5 +3,5 @@ const telegram = new Telegram(process.env.TOKEN);
 
 module.exports = () => {
     telegram.sendSticker(process.env.ADMIN, 'CAACAgIAAxkBAAEBeIFfjFTcuk_U3i39MPFG9rtwmZOmGQACAQEAAladvQoivp8OuMLmNBsE');
-    telegram.sendMessage(process.env.ADMIN, `🤩 *${bot.first_name}* bot has been started. Enjoy!`, { parse_mode: 'Markdown' });
+    telegram.sendMessage(process.env.ADMIN, `🤩 *The Higher Lower Game* bot has been started. Enjoy!`, { parse_mode: 'Markdown' });
 }
